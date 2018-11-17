3152ac0c932cdc28f97a5af48c89ad26aa907546
Greg Shepley <gregshepley@yahoo.co.uk>
2018-11-17 11:08:57 +0000

    added _create_or_update_dotenv to fabfile.py

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -1,7 +1,8 @@
+import random
 from fabric.contrib.files import exists
 from fabric.api import cd, env, local, run
 
-REPO_URL = 'https://github.com/hjwp/book-example.git'
+REPO_URL = 'https://gregcodethem@bitbucket.org/gregcodethem/langato.git'
 
 
 def deploy():
@@ -28,6 +29,16 @@ def _update_virtualenv():
         run(f'python3.6 -m venv virtualenv')
     run('./virtualenv/bin/pip install -r requirements.txt')
 
+
+def _create_or_update_dotenv():
+    append('.env', 'DJANGO_DEBUG_FALSE=y')  
+    append('.env', f'SITENAME={env.host}')
+    current_contents = run('cat .env')  
+    if 'DJANGO_SECRET_KEY' not in current_contents:  
+        new_secret = ''.join(random.SystemRandom().choices(  
+            'abcdefghijklmnopqrstuvwxyz0123456789', k=50
+        ))
+        append('.env', f'DJANGO_SECRET_KEY={new_secret}')
 
 def _update_static_files():
     run('./virtualenv/bin/python manage.py collectstatic --noinput')
