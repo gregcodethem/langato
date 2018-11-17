18cef4ee270381dc675bb2a508a38bb9dac8f421
Greg Shepley <gregshepley@yahoo.co.uk>
2018-11-17 09:40:01 +0000

    gitignore .env file and change elspeth in server_tools

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -8,13 +8,13 @@ def _get_manage_dot_py(host):
 
 def reset_database(host):
     manage_dot_py = _get_manage_dot_py(host)
-    with settings(host_string=f'elspeth@{host}'):
+    with settings(host_string=f'greg@{host}'):
         run(f'{manage_dot_py} flush --noinput')
 
 
 def create_session_on_server(host, email):
     manage_dot_py = _get_manage_dot_py(host)
-    with settings(host_string=f'elspeth@{host}'):
+    with settings(host_string=f'greg@{host}'):
         session_key = run(f'{manage_dot_py} create_session {email}')
         return session_key.strip()
 
